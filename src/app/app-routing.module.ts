d3d3e7b5ab3b9fafd85d18348b96ec410c82b430
LiLk <khalil.abbioui@gmail.com>
2022-11-24 14:36:58 +0100

    merge route success

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -20,15 +20,13 @@ const routes: Routes = [
     loadChildren: () => import('./un-bateau/un-bateau.module').then( m => m.UnBateauPageModule)
   },
   {
-<<<<<<< HEAD
     path: 'bateaux',
     loadChildren: () => import('./bateaux/bateaux.module').then( m => m.BateauxPageModule)
   },
-=======
+  {
     path: 'une-recette',
     loadChildren: () => import('./une-recette/une-recette.module').then( m => m.UneRecettePageModule)
   }
->>>>>>> 70316126ef487c85838c3555700ca29fa24b02a2
 ];
 @NgModule({
   imports: [
