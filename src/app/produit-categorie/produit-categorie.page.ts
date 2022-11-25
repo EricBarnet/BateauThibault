4ef2e5f22032cdd54c9ee4cb993be03c6ea6848d
LiLk <khalil.abbioui@gmail.com>
2022-11-25 16:13:35 +0100

    Liste produit categorie fonctionnelle

--- src/app/produit-categorie/produit-categorie.page.ts
+++ src/app/produit-categorie/produit-categorie.page.ts
@@ -1,19 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationExtras, Router } from '@angular/router';
+import { Categorie } from 'src/models/categorie';
 import { Product } from 'src/models/products';
 import { ProduitsService } from '../services/produits.service';
+
 @Component({
   selector: 'app-produit-categorie',
   templateUrl: './produit-categorie.page.html',
   styleUrls: ['./produit-categorie.page.scss'],
 })
 export class ProduitCategoriePage implements OnInit {
-  produitsList!: Product[];
-
-  constructor(private service:ProduitsService) { }
-
+  produitsList!: Product[]; 
+  categorieList: Categorie[] = [{name:"Poissons", id: 0}, {name:"Crustacés", id: 1}, {name:"Fruit de Mer", id:2}, {name:"Promotion", id: 3}];
+  constructor(private service:ProduitsService, private router: Router) { }
+  
   ngOnInit() {
-    this.service.getProducts().subscribe(res=>{
+    this.service.getProduits().subscribe(res=>{
       this.produitsList = res;
+      console.log(this.produitsList);
       },
       err =>{
         console.log("error")
@@ -21,5 +25,35 @@ export class ProduitCategoriePage implements OnInit {
     );
   }
 
+  test(){
+    console.log("toto");
+
+  }
+  
+  onLoadCategorieProduit(categorie: Categorie) {
+    console.log(categorie.name);
+    var listeProduitById = this.getProductsById(categorie.id);
+    let navigationExtras: NavigationExtras = {
+      state: {
+        produitsByIdCategorie: listeProduitById
+       }
+    };
+    this.router.navigate(['/selection-liste-produits'], navigationExtras);
+  }
+
+  getProductsById(id: number){
+    var produitCategorie = [];
+    console.log(id);
+    for(let p of this.produitsList){
+      console.log("entree dans le for");
+      console.log(p.category);
+      if(id == p.category){
+        console.log("test");
+        produitCategorie.push(p);
+      }
+    }
+    console.log(produitCategorie);
+    return produitCategorie;
+  }
 
 }
--- src/app/selection-liste-produits/selection-liste-produits.page.ts
+++ src/app/selection-liste-produits/selection-liste-produits.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute, Router } from '@angular/router';
+import { Product } from 'src/models/products';
+import { ProduitsService } from '../services/produits.service';
 @Component({
   selector: 'app-selection-liste-produits',
   templateUrl: './selection-liste-produits.page.html',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SelectionListeProduitsPage implements OnInit {
 
-  constructor() { }
+  produitsList!: Product[];
+  constructor(private route: ActivatedRoute, private service:ProduitsService, private router: Router) { }
 
   ngOnInit() {
+    this.route.queryParams.subscribe(params => {
+      if (this.router.getCurrentNavigation()!.extras.state) {
+        this.produitsList = this.router.getCurrentNavigation()!.extras.state!['produitsByIdCategorie'];
+        console.log(this.produitsList);
+      }
+    });
   }
 
 }
