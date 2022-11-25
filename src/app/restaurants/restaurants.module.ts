27a871f66af402777956602b49742098f5b676e5
Axel-A78 <71337761+Axel-A78@users.noreply.github.com>
2022-11-25 09:24:05 +0100

    Restaurants Page

---
+++ src/app/restaurants/restaurants.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { IonicModule } from '@ionic/angular';
+
+import { RestaurantsPageRoutingModule } from './restaurants-routing.module';
+
+import { RestaurantsPage } from './restaurants.page';
+import { SharedModule } from "../shared/shared.module";
+
+@NgModule({
+    declarations: [RestaurantsPage],
+    imports: [
+        CommonModule,
+        FormsModule,
+        IonicModule,
+        RestaurantsPageRoutingModule,
+        SharedModule
+    ]
+})
+export class RestaurantsPageModule {}
